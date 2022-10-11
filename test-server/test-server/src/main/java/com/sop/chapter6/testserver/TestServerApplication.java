8d43b44ebd5b29704cc102a43640d3551b0519ab
momim <63070014@kmitl.ac.th>
2022-10-11 16:04:08 +0700

    Initial commit

---
+++ test-server/test-server/src/main/java/com/sop/chapter6/testserver/TestServerApplication.java
@@ -0,0 +1,13 @@
+package com.sop.chapter6.testserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestServerApplication.class, args);
+	}
+
+}
